b14492df7040d16684a06aa9ac2b0a943ddb2a18
Nadya <Katara98Katara@gmail.com>
2019-02-04 12:09:36 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,4 @@
-javac -cp libs/log4j-1.2.17.jar -d classes -sourcepath ./sources sources/ua/edu/sumdu/j2se/volyk/tasks/*.java ^
-sources/ua/edu/sumdu/j2se/volyk/tasks/models/*.java ^
+javac -cp libs/log4j-1.2.17.jar -d classes -sourcepath ./sources sources/ua/edu/sumdu/j2se/volyk/tasks/models/*.java ^
 sources/ua/edu/sumdu/j2se/volyk/tasks/views/*.java ^
 sources/ua/edu/sumdu/j2se/volyk/tasks/controllers/*.java
 
